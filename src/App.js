b32d42fbf57585b38af5054f4c76c319eb57d587
Yoojin Hwang <dbwlshyj@gmail.com>
2021-02-16 19:23:38 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import Mainpage from "./main/index.js"
+import Mainpage from "./main/index.js";
+import { Route, Switch } from 'react-router-dom';
+import UploadPage from './upload'
+import ProductPage from './product'
+
 
 function App() {
   return (
     <div>
-      <Mainpage />
+      <Switch>
+      <Route exact={true} path="/">
+        <Mainpage /> 
+      </Route >
+      <Route exact={true} path="/product/:id">
+        <ProductPage />
+      </Route>
+      <Route exact={true} path="/upload">
+        <UploadPage />
+      </Route>
+     </Switch>
     </div>
   );
 }
--- src/main/index.js
+++ src/main/index.js
@@ -1,41 +1,59 @@
+import React from 'react';
 import './index.css';
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 
 function Mainpage(){
-    axios.get('https://cedbadfd-d874-42f2-af16-fa3afa683a8b.mock.pstmn.io/product')
-    .then(function(result){
-        console.log(result);
-    }).catch(function(error){
-        console.error(error);
-    })
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(function(){
+        axios.get('https://cedbadfd-d874-42f2-af16-fa3afa683a8b.mock.pstmn.io/product')
+        .then(function(result){
+            const products = result.data.products;
+            setProducts(products);
+        }).catch(function(error){
+            console.error(error);
+        })
+    },[]);
+   
 
     return (
-       <div> 
+    <div> 
+        <div> 
         <header>
-        <div id="header-area">
+         <div id="header-area">
             <img src="./img/1024px-Audi_logo_detail.svg.png" alt="logo" id="logo-img" />
-        </div>
+          </div>
         </header>
-        <div id="body">
+         <div id="body">
         <div id="banner">
         </div>
         <h1>판매상품</h1>
         <div id="product-list">
-            <div className="product-card">
-                <div>
-                    <img className="product-img" src="img/png-clipart-orange-basketball-basketball-ball-sports-basketball.png"/>
-                </div>
-                <div className="product-contents">
-                    <span className="product-name">농구공</span>
-                    <span className="product-price">50000원</span> 
-                    <div className="product-seller">
-                        <img className="product-avatar" src="img/580b57fcd9996e24bc43c53e.png" />
-                        <span>트위터</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+            {products.map(function(product, index){
+                    return (
+                      
+                     <div className="product-card">
+                        <Link className="product-link" to={`/product/${index}`}>
+                         <div>   
+                         <img className="product-img" src={product.imgUrl}/>
+                         </div>
+                         <div className="product-contents">
+                          <span className="product-name">{product.name}</span>
+                          <span className="product-price">{product.price}원</span> 
+                         <div className="product-seller">
+                            <img className="product-avatar" src="img/580b57fcd9996e24bc43c53e.png" />
+                            <span>트위터</span>
+                         </div>
+                         </div>
+                        </Link>
+                     </div>
+                     
+                    );    
+               })
+            }
+        </div>     
+         </div>
         </div>
         <footer>
         </footer>
